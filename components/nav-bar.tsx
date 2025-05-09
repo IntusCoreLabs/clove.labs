cf9f273c4c56663719c03ce2b43e199471dd2e5a
watercubz <eurysosagarcia@gmail.com>
2025-05-09 17:54:02 -0400

    feat(): add navbar

--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -1,8 +1,14 @@
-"use client"
-import { ThemeSelector } from "@/components/theme-selector"
-import Link from "next/link"
+"use client";
+
+import { ThemeSelector } from "@/components/theme-selector";
+import Link from "next/link";
+import { Button } from "./ui/button";
+import { useClerk, useUser } from "@clerk/nextjs";
 
 export function NavBar() {
+  const { signOut } = useClerk();
+  const { isSignedIn } = useUser();
+
   return (
     <header className="border-b p-4 flex items-center justify-between bg-background">
       <Link href="/" className="text-xl font-bold">
@@ -10,7 +16,17 @@ export function NavBar() {
       </Link>
       <div className="flex items-center gap-2">
         <ThemeSelector />
+        {isSignedIn && (
+          <Button
+            variant="destructive"
+            size="sm"
+            className="bg-destructive/90 hover:bg-primary"
+            onClick={() => signOut({ redirectUrl: "/" })}
+          >
+            Log out
+          </Button>
+        )}
       </div>
     </header>
-  )
+  );
 }
